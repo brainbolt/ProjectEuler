aa22807403d760aee5e3e7e8230b15baa878f1ed
brainbolt <brainbolts@gmail.com>
2017-06-20 17:17:28 -0700

    add 4

---
+++ js/4.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let h = 0;
+
+function getIsPalindrome(n) {
+  let s = n.toString();
+  return s === s.split('').reverse().join('');
+}
+
+for (let i = 999; i > 0; i--) {
+  for (let j = 999; j > 0; j--) {
+    let p = i * j;
+    if (getIsPalindrome(p)) {
+      if (p > h) h = p;
+    }
+  }
+}
+
+console.log(h);
